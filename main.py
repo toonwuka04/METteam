e61306aba5f88ca9fe6269f2f68032eb62c15fa3
Tobechi <tobechionwuka@gmail.com>
2021-08-13 23:24:27 -0400

    updated indexhtml

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = Flask(__name__, static_folder='static')
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/tutorspage")
+def tutorspage():
+    return render_template("tutorspage.html")
